72e3f57d8d42ad186bb8ad3fb0f15f773af0e462
Nabie_as63@itstep.edu.az <Nabie_as63@itstep.edu.az>
2024-11-11 01:09:25 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+//////////////////TASK1//////////////////////
+// const myName = prompt("Enter your name");
+// alert(myName);
+///////////////////////////////////////////
+
+//////////////////TASK2///////////////////////
+// let yearOfBirth = prompt("Enter year of birth");
+// alert(yearOfBirth);
+// const year = 2024;
+// const years = year - yearOfBirth;
+// console.log(years);
+///////////////////////////////////////////////
+
+/////////////////////TASK3///////////////////////////////
+// const dlina = prompt("Vvedite dlinu storon kvadrata");
+// alert(dlina);
+// const P = dlina * 4;
+// console.log("Perimetr kvadrata: ", P);
+/////////////////////////////////////////////////////////
+
+// //////////////////////TASK4////////////////////////////////
+// const r = prompt("Vvedite radius okrujnosti");
+// alert(r);
+// let s = 3.14 * r * r;
+// console.log("Plosad okrujnosti ravna :", s);
+/////////////////////////////////////////////////////////////
+
+////////////////////////////TASK5/////////////////////////
+// let distance = prompt("Vvedite rastoyaniye mejdu qorodami");
+// let time = prompt("Vvedite za skolko casov nujno dobratsa");
+// let s = distance / time;
+// console.log("Nujno exat s skoristyu - ", s);
+
+///////////////////////////TASK6//////////////////////////////
+// const dollar = prompt("Vvedite");
+// alert(dollar);
+// const euro = dollar * 0.93;
+// console.log("Euro: ",euro);
+
+/////////////////////////TASK7////////////////////////////////
+// let flashCartGB = prompt("Vvedite obyem fleshki v GB")
+// alert(flashCartGB);
+// let fileMb = 820;
+// let flashCartMB = flashCartGB * 1024;
+// let countFile = flashCartMB / fileMb;
+// console.log("Na fleshku pomestitsa ", countFile, "faylov")
+
+//////////////////////////TASK8////////////////////////////////
+// let wallet = prompt("Vvedite summu deneq v koseleke");
+// alert(wallet);
+// const oneChocolatePrice = prompt("VVedite summu sokolada");
+// alert(oneChocolatePrice);
+// let countChocolateBuy = wallet / oneChocolatePrice;
+// let change = wallet % oneChocolatePrice;
+// console.log("Vi mojete kupit ", countChocolateBuy, "sokolad","Sdaca - ",change);
+
+////////////////////////////TASK9///////////////////////////////////////////////
+// let number = prompt("Vvedite 3-znacnoye cislo");
+// let sotka = Math.floor(number / 100);
+// let desatka = Math.floor((number % 100) / 10);
+// let edinica = number % 10;
+// console.log("Sotki - ", sotka, "Desatki - ", desatka, "Edinici - ", edinica);
+// let reverseNumber = edinica * 100 + desatka * 10 + sotka;
+// console.log("Cislo zadom vpered - ", reverseNumber);
+
+///////////////////////////////TASK10//////////////////////////////////////////
+// const number = prompt("Vvedite celoye cislo");
+// const isEven = number % 2 == 0;
+// console.log(isEven || "Necetnoye");
